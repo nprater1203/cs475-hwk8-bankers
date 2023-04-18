98aaae7d85c86517a1b5bedddad3ad20a16de484
Owen Russell-Lanning <69214138+owen-russell-lanning@users.noreply.github.com>
2023-04-18 15:16:44 -0700

    Added File Injest Code

--- main.c
+++ main.c
@@ -5,6 +5,61 @@
 
 int main(int argc, char *argv[])
 {
+
+  // check we have a file path
+  if (argc  <2)
+  {
+    printf("No Input File Path Given\n");
+    return 0;
+  }
+
+  char *filePath = argv[1];
+
+  //try to open the file and make sure it exists
+  FILE * pFile;
+  pFile = fopen(filePath,"r");
+
+  if(pFile == NULL){
+    printf("Given File Path Not Found\n");
+    return 0;
+  }
+
+
+  int NRES; 	// number of resource types
+  int NPROC;	// number of processes
+  fscanf(pFile, "%d", &NRES);
+  fscanf(pFile, "%d", &NPROC);
+
+  int resource[3];
+
+  fscanf(pFile, "%d", &resource[0]);
+  fscanf(pFile, "%d", &resource[1]);
+  fscanf(pFile, "%d", &resource[2]);
+
+
+  //read in the demand matrix. as it's a fixed size we can specify params
+  int demand[5][3];
+
+  for(int i = 0; i< 15; i++){
+    int row = i / 3;
+    int col = i % 3;
+    fscanf(pFile, "%d", &demand[row][col]);
+
+  }
+
+  //read in alloc mat
+  int alloc[5][3];
+  for(int i = 0; i< 15; i++){
+    int row = i / 3;
+    int col = i % 3;
+    fscanf(pFile, "%d", &alloc[row][col]);
+
+  }
+
+
+
+
+
   // TODO: attempt to open scenario file and scan data into allocated structures
 
   // TODO: Run banker's safety algorithm
